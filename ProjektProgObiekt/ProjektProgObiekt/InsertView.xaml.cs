48e01e0c9f3931bae00a45133183faa202eec000
dfoltynski <dawidfoltynski7@gmail.com>
2022-06-30 00:58:12 +0200

    Added summary for InsertView

--- ProjektProgObiekt/ProjektProgObiekt/InsertView.xaml.cs
+++ ProjektProgObiekt/ProjektProgObiekt/InsertView.xaml.cs
@@ -27,14 +27,18 @@ namespace ProjektProgObiekt
             InitializeComponent();
             Load();
         }
-
+        /// <summary>
+        /// Metoda Load uruchamia wszystkie zawarte w niej metody odpowiedzialne za załadowanie roli, menadzerów i firm.
+        /// </summary>
         private void Load()
         {
             LoadRoles();
             LoadManagers();
             LoadCompanies();
         }
-
+        /// <summary>
+        /// Metoda LoadRoles wybiera wszystkie role z bazy danych, transferuje je na liste i za pomocą metody ForEach dynamicznie dodaje nowe wybory do ComboBox'a
+        /// </summary>
         private void LoadRoles()
         {
             _db.roles.ToList().ForEach(role =>
@@ -42,7 +46,9 @@ namespace ProjektProgObiekt
                 roleComboBox.Items.Add($"{role.role_name}");
             });
         }
-
+        /// <summary>
+        /// Metoda LoadManagers wybiera imię i nazwisko z bazy danych z tabeli managers, transferuje je na liste i za pomocą metody ForEach dynamicznie dodaje nowe wybory do ComboBox'a. W metodzie Add w ComboBox'ie dokonujemy konkatenacji dwóch stringów ze sobą i za pomocą klasy statycznej Regex podmieniamy niepotrzebne spacje, na tylko jedną.
+        /// </summary>
         private void LoadManagers()
         {
             _db.managers.ToList().ForEach(manager =>
@@ -50,7 +56,9 @@ namespace ProjektProgObiekt
                 managerComboBox.Items.Add(Regex.Replace($"{manager.name}{manager.last_name}", @"\s+", " "));
             });
         }
-         
+        /// <summary>
+        /// Metoda LoadCompanies wybiera wszystkie firmy z bazy danych, transferuje je na liste i za pomocą metody ForEach dynamicznie dodaje nowe wybory do ComboBox'a
+        /// </summary>
         private void LoadCompanies()
         {
             _db.companies.ToList().ForEach(company =>
@@ -60,7 +68,11 @@ namespace ProjektProgObiekt
         }
 
 
-
+        /// <summary>
+        /// Metoda Button_Click po kliknięciu deklaruje wartości, które będą używane do populacji kwerendy odpowiedzialnej za dodawanie użytkowników. Na końcu odswieżane są dane w taki sam sposób jak w Load w klasie MainWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string managerName = managerComboBox.Text.Trim().Split(' ')[0];
