ce9c55af052532915c866eeb9bb352158ddbb7b8
dfoltynski <dawidfoltynski7@gmail.com>
2022-06-30 00:52:07 +0200

    Added summary for MainWindow methods

--- ProjektProgObiekt/ProjektProgObiekt/MainWindow.xaml.cs
+++ ProjektProgObiekt/ProjektProgObiekt/MainWindow.xaml.cs
@@ -29,6 +29,9 @@ namespace ProjektProgObiekt
             Load();
         }
 
+        /// <summary>
+        /// Metoda Load za pomocą kwerendy pobiera nam wszystkie niezbędne informacje o pracowniku i łączy tabele.
+        /// </summary>
         private void Load()
         {
             myDataGrid.ItemsSource = (from em in _db.employees 
@@ -45,19 +48,24 @@ namespace ProjektProgObiekt
                                           manager = m.name + " " + m.last_name,
                                       }).ToList();
             dataGrid = myDataGrid;
-    //        db.Requests.Where(req => req.Code == code)
-    //.Include(req => req.Results) // Joining is performed here
-    //.Include(req => req.SomeOtherProperty)
-    //.ToList()
-        }
 
+        }
+        /// <summary>
+        /// Metoda insertBtn_Click po kliknięciu wywołuje metode ShowDialog na obiekcie klasy InsertView.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void insertBtn_Click(object sender, RoutedEventArgs e)
         {
             InsertView Insert = new InsertView();
 
             Insert.ShowDialog();
         }
-
+        /// <summary>
+        /// Metoda updateBtn_Click po kliknięciu wywołuje metode ShowDialog na obiekcie klasy UpdateView. Do konstruktora klasy UpdateView przesyłany jest ID pracownika, który zostal wybrany.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void updateBtn_Click(object sender, RoutedEventArgs e)
         {
             int id = (myDataGrid.SelectedItem as employee).id;
@@ -65,7 +73,11 @@ namespace ProjektProgObiekt
 
             Update.ShowDialog();
         }
-
+        /// <summary>
+        /// Metoda deleteBtn_Click po kliknięciu usuwa wybranego użytkownika z bazy danych i odswieża liste zawarta w myDataGrid w parametrze ItemsSource.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
             int id = (myDataGrid.SelectedItem as employee).id;
